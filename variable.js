22f3d5a864e249691adedcca9f31106540d19f26
Dhananjay Chaudhary <152369940+codebydjay@users.noreply.github.com>
2025-06-29 13:16:36 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,18 @@
+// let name = "Dhananjay";
+// let age = 22;
+// let isStudent = true;
+
+
+// console.log(`What's your name ${ name}` );
+// console.log(`your age is ${ age}` );
+// console.log(`you are student ${isStudent}` );
+// console.log(typeof(name));
+// console.log(typeof(name));
+let name = `Dhannajay`;
+let email = `rajina@gmail.com`;
+let gender = `male`;
+
+
+document.getElementById(`86`).textContent = name;
+document.getElementById(`68`).textContent = email;
+document.getElementById(`683 `).textContent = gender;
